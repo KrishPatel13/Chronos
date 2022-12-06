208cf753c0f467926f416a652c2bf9d56ef9a008
Krish Patel <kmppatel133@gmail.com>
2022-12-05 22:17:11 -0500

    Documented Event.java

--- model/CalendarModel.java
+++ model/CalendarModel.java
@@ -1,14 +1,11 @@
 package model;
 
 import event.Event;
-import javafx.scene.paint.Paint;
 import observer.EventObserver;
-
 
 import java.time.LocalDateTime;
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Calendar;
 
 
 public class CalendarModel implements Serializable {
@@ -17,13 +14,17 @@ public class CalendarModel implements Serializable {
     public String colour_font;
     private static ArrayList<EventObserver> completedGoals = new ArrayList<>();
 
-    public CalendarModel(){
-        this.events = new ArrayList<Event>();
 
+    public CalendarModel()
+    {
+        this.events = new ArrayList<Event>();
     }
+
+
     public CalendarModel(ArrayList<Event> events){
         this.events = events;
     }
+
 
     public static ArrayList<EventObserver> getCompletedGoals() {
         return completedGoals;
@@ -32,24 +33,27 @@ public class CalendarModel implements Serializable {
 
     public static void setCompletedGoals(ArrayList<EventObserver> oList) {completedGoals = oList;}
 
+
     public void addEvent(Event e)
     {
         this.events.add(e);
     }
 
+
     public ArrayList<Event> getAllEvents(){
         return this.events;
     }
 
-    public ArrayList<Event> getEventsInTime(LocalDateTime time) {
+
+    public ArrayList<Event> getEventsInTime(LocalDateTime time)
+    {
         ArrayList<Event> filteredEvents = new ArrayList<>();
-        for (Event e: this.events){
-            if (e.getTime().inTime(time)){
+        for (Event e: this.events)
+            if (e.getTimeBehaviour().inTime(time))
+            {
                 System.out.println(e.getName());
                 filteredEvents.add(e);
             }
-        }
         return filteredEvents;
     }
 }
-
