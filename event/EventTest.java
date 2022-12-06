1e88296580b99a520dd904c0e135db9a7ae67321
Krish Patel <kmppatel133@gmail.com>
2022-12-06 15:23:48 -0500

    Sanity Tests(new file EventTest.java added) for Event.java

---
+++ event/EventTest.java
@@ -0,0 +1,17 @@
+package event;
+
+import org.junit.jupiter.api.Test;
+
+
+/**
+ * A Unit Test Class for Event.java.
+ */
+class EventTest {
+    /**
+     * A Unit Test for Event.complete().
+     */
+    @Test
+    void complete() {
+
+    }
+}
